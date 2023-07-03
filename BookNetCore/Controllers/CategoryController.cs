26f604bcad6c39e5ff049032531b9f62ae12d96d
hieutrinh <hieutrinh358@gmail.com>
2023-07-03 12:02:32 -0500

    Add Category Controller with CREATE and UPDATE actions

---
+++ BookNetCore/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookNetCore.Data;
+using BookNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookNetCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category? obj)
+        {
+            //List<string>? names = _db.Categories.Select(a => a.Name.ToLower()).ToList();
+            //if(!String.IsNullOrEmpty(obj.Name.ToLower()))
+            //{
+            //    ModelState.AddModelError("name", "This name is already existed.");
+            //}
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);           
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) return NotFound();
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //List<string>? names = _db.Categories.Select(a => a.Name.ToLower()).ToList();
+            //if(!String.IsNullOrEmpty(obj.Name.ToLower()))
+            //{
+            //    ModelState.AddModelError("name", "This name is already existed.");
+            //}
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
+
--- BookNetCore/Program.cs
+++ BookNetCore/Program.cs
@@ -1,7 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BookNetCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("Default")
+    ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 
 var app = builder.Build();
 
